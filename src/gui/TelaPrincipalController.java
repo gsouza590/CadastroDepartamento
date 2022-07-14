48d12e8e8721499bc5a2361aef9f4390ed9a4bd4
Gabriel Souza <gsouza590@gmail.com>
2022-07-14 10:14:56 -0300

    Modificação Tela Principal e Controller

---
+++ src/gui/TelaPrincipalController.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.awt.MenuItem;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class TelaPrincipalController implements Initializable{
+
+	@FXML
+	private MenuItem menuItemVendedor;
+	
+	@FXML
+	private MenuItem menuItemDepartamento;
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	@FXML
+	public void onMenuItemVendedorAction() {
+		
+	}
+	
+	@FXML
+	public void onMenuItemDepartamentoAction() {
+		
+	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		
+	}
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
